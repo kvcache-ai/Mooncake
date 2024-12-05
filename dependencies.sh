cc54a566af663a521579ff94e5ec22b6d2a0ac10
wxsm <wxsms@foxmail.com>
2024-12-05 14:56:17 +0800

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -ex
 
 REPO_ROOT=`pwd`
 GITHUB_PROXY="https://mirror.ghproxy.com/github.com"
