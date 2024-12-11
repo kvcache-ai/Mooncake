5c68ccfd8ebe03c8b3861a78dbfa4d30bb5f7255
guokairui <guokairui1@wps.cn>
2024-12-11 16:30:51 +0800

    [Bugfix] build error when USE_CUDA

--- mooncake-transfer-engine/include/transport/cxl_transport/cxl_transport.h
+++ mooncake-transfer-engine/include/transport/cxl_transport/cxl_transport.h
@@ -58,7 +58,7 @@ class CxlTransport : public Transport {
     int install(std::string &local_server_name,
                 std::shared_ptr<TransferMetadata> meta, void **args) override;
 
-    int registerLocalMemory(void *addr, size_t length, const string &location,
+    int registerLocalMemory(void *addr, size_t length, const std::string &location,
                             bool remote_accessible,
                             bool update_metadata) override;
 
