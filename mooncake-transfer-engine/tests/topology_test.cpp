00ca6cd5b6ec0ae90ca4f6cb76c4bbc52aac066a
Nicholas <45984215+liusy58@users.noreply.github.com>
2025-01-10 15:31:27 +0800

    [TransferEngine] Add tests for `Topology` (#74)

--- mooncake-transfer-engine/tests/topology_test.cpp
+++ mooncake-transfer-engine/tests/topology_test.cpp
@@ -16,6 +16,76 @@ TEST(ToplogyTest, GetTopologyMatrix)
     ASSERT_EQ(topology.toString(), json_str);
 }
 
+TEST(ToplogyTest, TestEmpty) {
+  mooncake::Topology topology;
+  std::string json_str = "{\"cpu:0\" : [[\"erdma_0\"],[\"erdma_1\"]],\"cpu:1\" "
+                         ": [[\"erdma_1\"],[\"erdma_0\"]]}";
+  topology.clear();
+  topology.parse(json_str);
+  ASSERT_TRUE(!topology.empty());
+}
+
+TEST(ToplogyTest, TestHcaList) {
+  mooncake::Topology topology;
+  std::string json_str = "{\"cpu:0\" : [[\"erdma_0\"],[\"erdma_0\"]],\"cpu:1\" "
+                         ": [[\"erdma_0\"],[\"erdma_0\"]]}";
+  topology.clear();
+  topology.parse(json_str);
+  ASSERT_EQ(topology.getHcaList().size(), 1);
+  std::set<std::string> HcaList = {"erdma_0"};
+  for (auto &hca : topology.getHcaList()) {
+    ASSERT_TRUE(HcaList.count(hca));
+  }
+}
+
+TEST(ToplogyTest, TestHcaListSize) {
+  mooncake::Topology topology;
+  std::string json_str = "{\"cpu:0\" : [[\"erdma_0\"],[\"erdma_1\"]],\"cpu:1\" "
+                         ": [[\"erdma_2\"],[\"erdma_3\"]]}";
+  topology.clear();
+  topology.parse(json_str);
+  ASSERT_EQ(topology.getHcaList().size(), 4);
+}
+
+TEST(ToplogyTest, TestHcaList2) {
+  mooncake::Topology topology;
+  std::string json_str = "{\"cpu:0\" : [[\"erdma_0\"],[\"erdma_1\"]],\"cpu:1\" "
+                         ": [[\"erdma_1\"],[\"erdma_0\"]]}";
+  topology.clear();
+  topology.parse(json_str);
+  ASSERT_EQ(topology.getHcaList().size(), 2);
+  std::set<std::string> HcaList = {"erdma_0", "erdma_1"};
+  for (auto &hca : topology.getHcaList()) {
+    ASSERT_TRUE(HcaList.count(hca));
+  }
+}
+
+TEST(ToplogyTest, TestMatrix) {
+  mooncake::Topology topology;
+  std::string json_str = "{\"cpu:0\" : [[\"erdma_0\"],[\"erdma_1\"]]}";
+  topology.clear();
+  topology.parse(json_str);
+  auto matrix = topology.getMatrix();
+  ASSERT_TRUE(matrix.size() == 1);
+  ASSERT_TRUE(matrix.count("cpu:0"));
+}
+
+TEST(ToplogyTest, TestSelectDevice) {
+  mooncake::Topology topology;
+  std::string json_str = "{\"cpu:0\" : [[\"erdma_0\"],[\"erdma_1\"]]}";
+  topology.clear();
+  topology.parse(json_str);
+  std::set<int> items = {0, 1};
+  int device;
+  device = topology.selectDevice("cpu:0", 2);
+  ASSERT_TRUE(items.count(device));
+  items.erase(device);
+  device = topology.selectDevice("cpu:0", 1);
+  ASSERT_TRUE(items.count(device));
+  items.erase(device);
+  ASSERT_TRUE(items.empty());
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
