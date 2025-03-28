07fb45cbf3f6d339a330a4d61ff61250ca19b24a
xhy <fujunjie1@qq.com>
2025-03-28 18:56:00 +0800

    fix name error

--- mooncake-wheel/setup.py
+++ mooncake-wheel/setup.py
@@ -15,7 +15,7 @@ setup(
     package_data={"mooncake": [
         "mooncake_vllm_adaptor.cpython-310-x86_64-linux-gnu.so",
         "mooncake_master",
-        "lib/libetcd-cpp-api.so",
+        "lib_so/libetcd-cpp-api.so",
     ]},
     include_package_data=True,
     zip_safe=False,
