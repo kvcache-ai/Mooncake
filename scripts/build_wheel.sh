092740ea94bef91811bba97726ef2719135285a1
sjy <751080330@qq.com>
2025-04-11 11:34:25 +0800

    ci: add matrix strategy for testing on multiple ubuntu versions

--- scripts/build_wheel.sh
+++ scripts/build_wheel.sh
@@ -39,7 +39,46 @@ mkdir -p repaired_wheels
 
 echo "Repairing wheel with auditwheel..."
 # Use auditwheel to repair the wheel and include missing .so files
-auditwheel repair dist/*.whl -w repaired_wheels/ --plat manylinux_2_35_x86_64
+auditwheel repair dist/*.whl \
+--exclude libcurl.so* \
+--exclude libibverbs.so* \
+--exclude libnuma.so* \
+--exclude libstdc++.so* \
+--exclude libgcc_s.so* \
+--exclude libc.so* \
+--exclude libnghttp2.so* \
+--exclude libidn2.so* \
+--exclude librtmp.so* \
+--exclude libssh.so* \
+--exclude libpsl.so* \
+--exclude libssl.so* \
+--exclude libcrypto.so* \
+--exclude libgssapi_krb5.so* \
+--exclude libldap.so* \
+--exclude liblber.so* \
+--exclude libbrotlidec.so* \
+--exclude libz.so* \
+--exclude libnl-route-3.so* \
+--exclude libnl-3.so* \
+--exclude libm.so* \
+--exclude liblzma.so* \
+--exclude libunistring.so* \
+--exclude libgnutls.so* \
+--exclude libhogweed.so* \
+--exclude libnettle.so* \
+--exclude libgmp.so* \
+--exclude libkrb5.so* \
+--exclude libk5crypto.so* \
+--exclude libcom_err.so* \
+--exclude libkrb5support.so* \
+--exclude libsasl2.so* \
+--exclude libbrotlicommon.so* \
+--exclude libp11-kit.so* \
+--exclude libtasn1.so* \
+--exclude libkeyutils.so* \
+--exclude libresolv.so* \
+--exclude libffi.so* \
+-w repaired_wheels/
 
 # Replace original wheel with repaired wheel
 rm -f dist/*.whl
