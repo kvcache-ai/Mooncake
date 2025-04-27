a9484a5fba0d9e81fb2b8ee87f817add5e0bdfa9
susun <751080330@qq.com>
2025-04-27 16:16:17 +0800

    feat(types): add new error codes to errorCodeMap

--- mooncake-store/src/types.cpp
+++ mooncake-store/src/types.cpp
@@ -5,6 +5,7 @@ namespace mooncake {
 const std::string& toString(ErrorCode errorCode) noexcept {
     static const std::unordered_map<ErrorCode, std::string> errorCodeMap = {
         {ErrorCode::OK, "OK"},
+        {ErrorCode::INTERNAL_ERROR, "INTERNAL_ERROR"},
         {ErrorCode::BUFFER_OVERFLOW, "BUFFER_OVERFLOW"},
         {ErrorCode::SHARD_INDEX_OUT_OF_RANGE, "SHARD_INDEX_OUT_OF_RANGE"},
         {ErrorCode::AVAILABLE_SEGMENT_EMPTY, "AVAILABLE_SEGMENT_EMPTY"},
@@ -16,7 +17,11 @@ const std::string& toString(ErrorCode errorCode) noexcept {
         {ErrorCode::INVALID_WRITE, "INVALID_WRITE"},
         {ErrorCode::INVALID_READ, "INVALID_READ"},
         {ErrorCode::INVALID_REPLICA, "INVALID_REPLICA"},
+        {ErrorCode::REPLICA_IS_NOT_READY, "REPLICA_IS_NOT_READY"},
+        {ErrorCode::OBJECT_NOT_FOUND, "OBJECT_NOT_FOUND"},
+        {ErrorCode::OBJECT_ALREADY_EXISTS, "OBJECT_ALREADY_EXISTS"},
         {ErrorCode::TRANSFER_FAIL, "TRANSFER_FAIL"},
+        {ErrorCode::RPC_FAIL, "RPC_FAIL"},
     };
 
     auto it = errorCodeMap.find(errorCode);
