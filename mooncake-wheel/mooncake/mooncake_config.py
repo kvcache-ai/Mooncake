0d3bb196dd4af52a7acf199ab5f4861bb055141a
baoloongmao <baoloongmao@tencent.com>
2025-05-09 09:43:49 +0800

    supply a MooncakeConfig into whl

---
+++ mooncake-wheel/mooncake/mooncake_config.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Mooncake Configuration
+"""
+import json
+import os
+from dataclasses import dataclass
+
+@dataclass
+class MooncakeConfig:
+    local_hostname: str
+    metadata_server: str
+    global_segment_size: int
+    local_buffer_size: int
+    protocol: str
+    device_name: str
+    master_server_address: str
+
+    @staticmethod
+    def from_file(file_path: str) -> 'MooncakeConfig':
+        """Load the config from a JSON file."""
+        with open(file_path) as fin:
+            config = json.load(fin)
+        return MooncakeConfig(
+            local_hostname=config.get("local_hostname"),
+            metadata_server=config.get("metadata_server"),
+            global_segment_size=config.get("global_segment_size", 3355443200),
+            local_buffer_size=config.get("local_buffer_size", 1073741824),
+            protocol=config.get("protocol", "tcp"),
+            device_name=config.get("device_name", ""),
+            master_server_address=config.get("master_server_address"),
+        )
+
+    @staticmethod
+    def load_from_env() -> 'MooncakeConfig':
+        """Load config from a file specified in the environment variable."""
+        config_file_path = os.getenv('MOONCAKE_CONFIG_PATH')
+        if config_file_path is None:
+            raise ValueError(
+                "The environment variable 'MOONCAKE_CONFIG_PATH' is not set.")
+        return MooncakeConfig.from_file(config_file_path)
