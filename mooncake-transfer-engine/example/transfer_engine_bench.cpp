47717aa9dbe88430b06c9584f4d5aef921c6a24b
Feng Ren <alogfans@gmail.com>
2025-06-10 02:02:11 +0000

    change bench alloc/free

--- mooncake-transfer-engine/example/transfer_engine_bench.cpp
+++ mooncake-transfer-engine/example/transfer_engine_bench.cpp
@@ -99,8 +99,12 @@ static void *allocateMemoryPool(size_t size, int socket_id,
         int gpu_id = FLAGS_gpu_id;
         void *d_buf;
         checkCudaError(cudaSetDevice(gpu_id), "Failed to set device");
+#ifdef USE_NVLINK
+        d_buf = mooncake::NvlinkTransport::allocatePinnedLocalMemory(size);
+#else
         checkCudaError(cudaMalloc(&d_buf, size),
                        "Failed to allocate device memory");
+#endif
         return d_buf;
     }
 #endif
@@ -109,6 +113,14 @@ static void *allocateMemoryPool(size_t size, int socket_id,
 
 static void freeMemoryPool(void *addr, size_t size) {
 #ifdef USE_CUDA
+#ifdef USE_NVLINK
+    CUmemGenericAllocationHandle handle;
+    auto result = cuMemRetainAllocationHandle(&handle, ptr);
+    if (result == CUDA_SUCCESS) {
+        mooncake::NvlinkTransport::freePinnedLocalMemory(addr);
+        return;
+    }
+#endif
     // check pointer on GPU
     cudaPointerAttributes attributes;
     checkCudaError(cudaPointerGetAttributes(&attributes, addr),
@@ -299,12 +311,7 @@ int initiator() {
     buffer_num = FLAGS_use_vram ? 1 : NR_SOCKETS;
     if (FLAGS_use_vram) LOG(INFO) << "VRAM is used";
     for (int i = 0; i < buffer_num; ++i) {
-#ifdef USE_NVLINK
-        addr[i] = mooncake::NvlinkTransport::allocatePinnedLocalMemory(
-            FLAGS_buffer_size);
-#else
         addr[i] = allocateMemoryPool(FLAGS_buffer_size, i, FLAGS_use_vram);
-#endif
         std::string name_prefix = FLAGS_use_vram ? "cuda:" : "cpu:";
         int rc = engine->registerLocalMemory(addr[i], FLAGS_buffer_size,
                                              name_prefix + std::to_string(i));
@@ -351,11 +358,7 @@ int initiator() {
 
     for (int i = 0; i < buffer_num; ++i) {
         engine->unregisterLocalMemory(addr[i]);
-#ifdef USE_NVLINK
-        mooncake::NvlinkTransport::freePinnedLocalMemory(addr[i]);
-#else
         freeMemoryPool(addr[i], FLAGS_buffer_size);
-#endif
     }
 
     return 0;
