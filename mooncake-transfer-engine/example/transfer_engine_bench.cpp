430ac36aba25e07d200bceb7c87d790a32f025a9
Feng Ren <alogfans@gmail.com>
2025-06-11 11:45:18 +0000

    fix bug in transfer engine bench

--- mooncake-transfer-engine/example/transfer_engine_bench.cpp
+++ mooncake-transfer-engine/example/transfer_engine_bench.cpp
@@ -404,12 +404,7 @@ int target() {
 #ifdef USE_CUDA
     if (FLAGS_use_vram) LOG(INFO) << "VRAM is used";
     for (int i = 0; i < buffer_num; ++i) {
-#ifdef USE_NVLINK
-        addr[i] = mooncake::NvlinkTransport::allocatePinnedLocalMemory(
-            FLAGS_buffer_size);
-#else
         addr[i] = allocateMemoryPool(FLAGS_buffer_size, i, FLAGS_use_vram);
-#endif
         std::string name_prefix = FLAGS_use_vram ? "cuda:" : "cpu:";
         int rc = engine->registerLocalMemory(addr[i], FLAGS_buffer_size,
                                              name_prefix + std::to_string(i));
