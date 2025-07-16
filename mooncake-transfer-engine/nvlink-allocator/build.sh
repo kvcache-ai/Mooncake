2065eb1a7bff6e8fafcff2c043167fd6b7c4b35d
ishandhanani <ishandhanani@gmail.com>
2025-07-15 19:56:22 -0700

    swap

--- mooncake-transfer-engine/nvlink-allocator/build.sh
+++ mooncake-transfer-engine/nvlink-allocator/build.sh
@@ -11,7 +11,7 @@ mkdir -p "$OUTPUT_DIR"
 
 CPP_FILE=$(dirname $(readlink -f $0))/nvlink_allocator.cpp  # get cpp file path, under same dir with this script
 
-g++ "$CPP_FILE" -o "$OUTPUT_DIR/nvlink_allocator.so" --shared -fPIC -lcuda -lcudart -I/usr/local/cuda/include
+g++ "$CPP_FILE" -o "$OUTPUT_DIR/nvlink_allocator.so" --shared -fPIC -lcuda -I/usr/local/cuda/include
 
 if [ $? -eq 0 ]; then
     echo "Successfully built nvlink_allocator.so in $OUTPUT_DIR"
