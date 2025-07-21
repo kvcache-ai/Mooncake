7615ea8bdae1f7d2f52f17c9dd7ae3178aebf159
ykwd <oneday117@qq.com>
2025-07-21 17:02:26 +0800

    [Fix] Support global_segment_size larger than 64GB (#647)
    When global_segment_size is larger than max_mr_size (depends on device, usually is 64GB), split it into multiple segments to mount.

--- mooncake-integration/store/store_py.cpp
+++ mooncake-integration/store/store_py.cpp
@@ -12,6 +12,7 @@
 #include "client_buffer.hpp"
 #include "types.h"
 #include "utils.h"
+#include "config.h"
 
 namespace py = pybind11;
 
@@ -170,22 +171,31 @@ int DistributedObjectStore::setup(const std::string &local_hostname,
                    << toString(result.error());
         return 1;
     }
-    // Skip mount segment if global_segment_size is 0
-    if (global_segment_size == 0) {
-        return 0;
-    }
-    void *ptr = allocate_buffer_allocator_memory(global_segment_size);
-    if (!ptr) {
-        LOG(ERROR) << "Failed to allocate segment memory";
-        return 1;
-    }
-    segment_ptr_.reset(ptr);
-    auto mount_result =
-        client_->MountSegment(segment_ptr_.get(), global_segment_size);
-    if (!mount_result.has_value()) {
-        LOG(ERROR) << "Failed to mount segment: "
-                   << toString(mount_result.error());
-        return 1;
+
+    // If global_segment_size is 0, skip mount segment;
+    // If global_segment_size is larger than max_mr_size, split to multiple
+    // segments.
+    auto max_mr_size = globalConfig().max_mr_size; // Max segment size
+    uint64_t total_glbseg_size = global_segment_size;  // For logging
+    uint64_t current_glbseg_size = 0;                  // For logging
+    while (global_segment_size > 0) {
+        size_t segment_size = std::min(global_segment_size, max_mr_size);
+        global_segment_size -= segment_size;
+        current_glbseg_size += segment_size;
+        LOG(INFO) << "Mounting segment: " << segment_size << " bytes, "
+                  << current_glbseg_size << " of " << total_glbseg_size;
+        void *ptr = allocate_buffer_allocator_memory(segment_size);
+        if (!ptr) {
+            LOG(ERROR) << "Failed to allocate segment memory";
+            return 1;
+        }
+        segment_ptrs_.emplace_back(ptr);
+        auto mount_result = client_->MountSegment(ptr, segment_size);
+        if (!mount_result.has_value()) {
+            LOG(ERROR) << "Failed to mount segment: "
+                       << toString(mount_result.error());
+            return 1;
+        }
     }
 
     return 0;
@@ -211,7 +221,7 @@ int DistributedObjectStore::tearDownAll() {
     // Reset all resources
     client_.reset();
     client_buffer_allocator_.reset();
-    segment_ptr_.reset();
+    segment_ptrs_.clear();
     local_hostname = "";
     device_name = "";
     protocol = "";
