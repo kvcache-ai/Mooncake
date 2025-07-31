9dd6d5d37c15fad9d46367bab5d72a5efcdd4144
Ke Yang <oneday117@qq.com>
2025-07-31 12:14:36 +0000

    check length when register memory

--- mooncake-transfer-engine/src/transfer_engine.cpp
+++ mooncake-transfer-engine/src/transfer_engine.cpp
@@ -303,6 +303,10 @@ int TransferEngine::registerLocalMemory(void *addr, size_t length,
             << "Transfer Engine does not support overlapped memory region";
         return ERR_ADDRESS_OVERLAPPED;
     }
+    if (length == 0) {
+        LOG(ERROR) << "Transfer Engine does not support zero length memory region";
+        return ERR_INVALID_ARGUMENT;
+    }
     for (auto transport : multi_transports_->listTransports()) {
         int ret = transport->registerLocalMemory(
             addr, length, location, remote_accessible, update_metadata);
