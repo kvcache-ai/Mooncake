185c5d229ba6dd7f8e2a871b9d73fd8765b3af5c
qicosmos <qicosmos@linux.alibaba.com>
2025-09-02 00:40:21 +0800

    [coro_rpc] use client pool and enable rdma (#789)

--- scripts/ascend/dependencies_ascend_installation.sh
+++ scripts/ascend/dependencies_ascend_installation.sh
@@ -107,7 +107,7 @@ export CPLUS_INCLUDE_PATH=$(echo $CPLUS_INCLUDE_PATH | tr ':' '\n' | grep -v "/u
 # Install yalantinglibs
 clone_repo_if_not_exists "yalantinglibs" "https://github.com/alibaba/yalantinglibs.git"
 cd yalantinglibs || exit
-git checkout 0.5.1
+git checkout 0.5.5
 rm -rf build
 mkdir -p build && cd build
 cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARK=OFF -DBUILD_UNIT_TESTS=OFF
