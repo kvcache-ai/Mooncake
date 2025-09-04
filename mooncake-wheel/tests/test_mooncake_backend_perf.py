9554390f82b4cf4672103b297b2ab27422dfcdcd
UNIDY <unidy2002@outlook.com>
2025-09-04 15:13:02 +0800

    Perf test

--- mooncake-wheel/tests/test_mooncake_backend_perf.py
+++ mooncake-wheel/tests/test_mooncake_backend_perf.py
@@ -38,13 +38,13 @@ def run_latency_test(rank, world_size, backend, device, collective, data_size, r
     # Calculate average time
     avg_time = (end - start) / num_iterations
 
-    dist.destroy_process_group()  # Destroy the process group after testing
-
     # Store the result
     results[rank] = avg_time
 
     while len(results) < world_size:
         time.sleep(1)
+
+    dist.destroy_process_group()  # Destroy the process group after testing
 
 class TestMooncakeBackendPerf(unittest.TestCase):
     def setUp(self):
