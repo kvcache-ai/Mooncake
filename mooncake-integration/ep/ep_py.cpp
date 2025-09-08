bba4f933efaf76075cb99e9f15e6cc93381a6905
UNIDY <unidy2002@outlook.com>
2025-09-08 16:02:53 +0800

    Test

--- mooncake-integration/ep/ep_py.cpp
+++ mooncake-integration/ep/ep_py.cpp
@@ -17,6 +17,11 @@ namespace py = pybind11;
 
 namespace mooncake {
 
+void testBinding1(c10d::DistributedBackendOptions distBackendOpts) {}
+
+void testBinding2(c10::intrusive_ptr<MooncakeBackend::MooncakeBackendOptions>
+                      backendOptions) {}
+
 c10::intrusive_ptr<c10d::Backend> createMooncakeBackend(
     c10d::DistributedBackendOptions distBackendOpts,
     c10::intrusive_ptr<MooncakeBackend::MooncakeBackendOptions>
@@ -57,6 +62,8 @@ std::string getPreferredHca(c10::intrusive_ptr<c10d::Backend> backend,
 }
 
 PYBIND11_MODULE(ep, m) {
+    m.def("test_binding_1", &testBinding1);
+    m.def("test_binding_2", &testBinding2);
     m.def("createMooncakeBackend", &createMooncakeBackend);
     m.def("createMooncakeCpuBackend", &createMooncakeCpuBackend);
     m.def("set_host_ip", &MooncakeBackend::setHostIp);
