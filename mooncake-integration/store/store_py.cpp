b53341ea77c750ec8adda2bb63a6164a1383dd7c
shichangzhang064 <zhangshichang@h-partners.com>
2025-11-04 16:10:57 +0800

    feat(Store): add client local hot cache log to show performance

--- mooncake-integration/store/store_py.cpp
+++ mooncake-integration/store/store_py.cpp
@@ -285,7 +285,7 @@ class MooncakeStorePyWrapper {
                     data ? pybind11::bytes((char *)data->ptr(), data->size())
                          : kNullString);
             }
-
+            
             return results;
         }
     }
