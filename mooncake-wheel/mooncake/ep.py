bab99659d05c955c9568e3db649d2b3143f0c4a7
UNIDY <unidy2002@outlook.com>
2025-11-24 18:56:52 +0800

    Debug

--- mooncake-wheel/mooncake/ep.py
+++ mooncake-wheel/mooncake/ep.py
@@ -5,5 +5,12 @@ import torch
 
 torch_version = Version(torch.__version__).base_version
 version_suffix = "_" + torch_version.replace(".", "_")
-backend_module = importlib.import_module("mooncake.ep" + version_suffix)
+
+try:
+    backend_module = importlib.import_module("mooncake.ep" + version_suffix)
+except ModuleNotFoundError:
+    raise ImportError(
+        f"Mooncake EP was not built against torch=={torch_version}.\n"
+        f"Open an issue at https://github.com/kvcache-ai/Mooncake/issues."
+    )
 globals().update({k: v for k, v in backend_module.__dict__.items() if not k.startswith("_")})
