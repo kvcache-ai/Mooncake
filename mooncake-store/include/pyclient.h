2d1d426da3933a0e1a0a574dc377e0c75ecdbee1
Xingrui Yi <yixingrui@linux.alibaba.com>
2025-12-08 14:36:20 +0800

    [Store]: Introduce shm helper for dummy
    Signed-off-by: Xingrui Yi <yixingrui@linux.alibaba.com>

--- mooncake-store/include/pyclient.h
+++ mooncake-store/include/pyclient.h
@@ -21,7 +21,6 @@ struct ShmRegisterRequest {
     uint64_t dummy_base_addr;
     uint64_t shm_size;
     uint64_t local_buffer_size;
-    char shm_name[256];
 };
 
 // Python-specific wrapper class for client interface
