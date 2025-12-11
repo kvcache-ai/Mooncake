1c428a88d814d979cbf7c7b43ef13e713fe6960c
Anatolii Rozanov <arozanov@amd.com>
2025-12-11 09:15:53 +0000

    [TE/Examples] Remove unnecessary copyrights

--- mooncake-transfer-engine/example/transfer_engine_bench_with_notify.cpp
+++ mooncake-transfer-engine/example/transfer_engine_bench_with_notify.cpp
@@ -11,9 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// Modifications Copyright(C) 2025 Advanced Micro Devices, Inc.
-// All rights reserved.
 
 #include <gflags/gflags.h>
 #include <glog/logging.h>
