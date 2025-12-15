fb54c20bd1a75e7dce1c3f887ba3067f38e043e5
百麒 <yaozhong.lyz@alibaba-inc.com>
2025-12-15 11:23:30 +0800

    delete debug log in metadata_plugin.cpp

--- mooncake-transfer-engine/src/transfer_metadata_plugin.cpp
+++ mooncake-transfer-engine/src/transfer_metadata_plugin.cpp
@@ -534,9 +534,7 @@ std::pair<std::string, std::string> parseConnectionString(
 std::shared_ptr<MetadataStoragePlugin> MetadataStoragePlugin::Create(
     const std::string &conn_string) {
     auto parsed_conn_string = parseConnectionString(conn_string);
-    LOG(INFO) << "The value of parse conn string: " << parsed_conn_string.first;
 #ifdef USE_ETCD
-    LOG(INFO) << "Inside USE_ETCD";
     if (parsed_conn_string.first == "etcd") {
         return std::make_shared<EtcdStoragePlugin>(parsed_conn_string.second);
     }
