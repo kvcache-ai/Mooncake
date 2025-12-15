fc6d676302c73da0555ddd4c9b51b80a56330863
Feng Ren <alogfans@gmail.com>
2025-12-15 07:32:19 +0000

    Fix build bug

--- mooncake-transfer-engine/benchmark/tev0_backend.cpp
+++ mooncake-transfer-engine/benchmark/tev0_backend.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fstream>
+
 #include "tev0_backend.h"
 #include "utils.h"
 #include "common.h"
