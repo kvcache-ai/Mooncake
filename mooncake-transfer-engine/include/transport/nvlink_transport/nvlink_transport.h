ddb3a3f8d26b655a066783fe6f0bfa96986ab037
百麒 <yaozhong.lyz@alibaba-inc.com>
2025-12-18 11:42:55 +0000

    Change supportFabricMem logic to support fallback CudaMalloc when CuMemCreate file for Fabric handle
    change format

--- mooncake-transfer-engine/include/transport/nvlink_transport/nvlink_transport.h
+++ mooncake-transfer-engine/include/transport/nvlink_transport/nvlink_transport.h
@@ -40,8 +40,6 @@ class NvlinkTransport : public Transport {
 
     static void* allocatePinnedLocalMemory(size_t length);
 
-    static void* allocateViaCudaMalloc(size_t length);
-
     static void freePinnedLocalMemory(void* addr);
 
    protected:
