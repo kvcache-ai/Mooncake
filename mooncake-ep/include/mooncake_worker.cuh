3d8d8f4372a79ef4ce8c0ebd567fd3130cde5292
Xun Sun <UNIDY2002@outlook.com>
2025-12-22 14:10:40 +0800

    [EP] Implement elastic scaling up (#1173)

--- mooncake-ep/include/mooncake_worker.cuh
+++ mooncake-ep/include/mooncake_worker.cuh
@@ -11,6 +11,9 @@
 
 namespace mooncake {
 
+static constexpr size_t kBufferSize = 1u << 24;
+static constexpr size_t kMaxNumRanks = 64;
+
 struct TransferGroupMeta {
     int rank;
     int size;
@@ -18,10 +21,13 @@ struct TransferGroupMeta {
     bool* activeRanks;
     bool* activeRanksDevice;
     at::Tensor activeRanksTensor;
+    bool peerConnected[kMaxNumRanks]{};
     TransferEngine* engine;
+    c10::intrusive_ptr<::c10d::Store> store;
     int bufferBaseIndex;
-    std::vector<TransferMetadata::SegmentID> segmentIDs;
-    std::vector<std::shared_ptr<TransferMetadata::SegmentDesc>> segmentDescs;
+    int backendIndex;
+    TransferMetadata::SegmentID segmentIDs[kMaxNumRanks];
+    std::shared_ptr<TransferMetadata::SegmentDesc> segmentDescs[kMaxNumRanks];
 };
 
 __global__ struct Task {
@@ -34,15 +40,12 @@ __global__ struct Task {
     void* transferGroupMeta;
 };
 
-static constexpr size_t kBufferSize = 1u << 24;
-static constexpr size_t kMaxNumRanks = 64;
-
 void launchReduceKernel(at::Tensor dst, size_t pos, size_t realSize, void* src,
                         size_t numRanks, c10d::ReduceOp op, bool* activeRanks,
                         cudaStream_t stream);
 
 void launchReduceCpu(at::Tensor dst, size_t pos, size_t realSize, void* src,
-                     size_t numRanks, c10d::ReduceOp op);
+                     size_t numRanks, c10d::ReduceOp op, bool* activeRanks);
 
 class MooncakeWorker {
    public:
