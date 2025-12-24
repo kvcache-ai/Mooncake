944436bd0af5c7e10ba2762edf4fdeb38ecb6ece
Cruz Zhao <CruzZhao@linux.alibaba.com>
2025-12-24 20:01:14 +0800

    fix
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- mooncake-store/src/client_service.cpp
+++ mooncake-store/src/client_service.cpp
@@ -1861,16 +1861,12 @@ ErrorCode Client::FindFirstCompleteReplica(
 
 tl::expected<Replica::Descriptor, ErrorCode> Client::GetPreferredReplica(
     const std::vector<Replica::Descriptor>& replica_list) {
-    if (!mounted_segments_.size()) return replica_list[0];
     if (replica_list.empty()) {
         return tl::make_unexpected(ErrorCode::INVALID_PARAMS);
     }
-
-    auto first_complete_it =
-        std::find_if(replica_list.begin(), replica_list.end(),
-                     [](const Replica::Descriptor& r) {
-                         return r.status == ReplicaStatus::COMPLETE;
-                     });
+    if (mounted_segments_.empty() || replica_list.size() == 1) {
+        return replica_list[0];
+    }
 
     std::unordered_set<std::string> local_endpoints;
     local_endpoints.reserve(mounted_segments_.size());
@@ -1887,10 +1883,6 @@ tl::expected<Replica::Descriptor, ErrorCode> Client::GetPreferredReplica(
                 return rep;
             }
         }
-    }
-
-    if (first_complete_it != replica_list.end()) {
-        return *first_complete_it;
     }
 
     return replica_list[0];
