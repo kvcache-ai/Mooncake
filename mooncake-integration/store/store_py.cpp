a9365f987f928baed47a35e9507c6eee0e87aa52
Cruz Zhao <CruzZhao@linux.alibaba.com>
2026-01-26 20:52:48 +0800

    support engram
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- mooncake-integration/store/store_py.cpp
+++ mooncake-integration/store/store_py.cpp
@@ -11,6 +11,15 @@
 
 #include "integration_utils.h"
 
+// Forward declaration for Engram bindings
+namespace mooncake {
+namespace engram {
+void bind_engram(py::module &m);
+}
+}  // namespace mooncake
+#include "engram/engram.h"
+#include "engram/engram_config.h"
+
 namespace py = pybind11;
 
 namespace mooncake {
@@ -1046,6 +1055,28 @@ PYBIND11_MODULE(store, m) {
 
     // Create a wrapper that exposes DistributedObjectStore with Python-specific
     // methods
+    // Helper function to extract PyClient shared_ptr from
+    // MooncakeStorePyWrapper This is used by Engram to get the underlying
+    // PyClient We return it as a Python capsule to avoid type registration
+    // issues
+    m.def(
+        "_get_pyclient_from_wrapper",
+        [](MooncakeStorePyWrapper &wrapper) -> py::object {
+            if (!wrapper.store_) {
+                return py::none();
+            }
+            // Return as a capsule containing the shared_ptr
+            // The caller (engram_py.cpp) will extract it
+            std::shared_ptr<PyClient> *ptr =
+                new std::shared_ptr<PyClient>(wrapper.store_);
+            return py::capsule(ptr, [](void *p) {
+                delete static_cast<std::shared_ptr<PyClient> *>(p);
+            });
+        },
+        py::arg("wrapper"),
+        "Get PyClient from MooncakeDistributedStore (internal use, returns "
+        "capsule)");
+
     py::class_<MooncakeStorePyWrapper>(m, "MooncakeDistributedStore")
         .def(py::init<>())
         .def(
@@ -1653,6 +1684,9 @@ PYBIND11_MODULE(store, m) {
         py::arg("node"),
         "Bind the current thread and memory allocation preference to the "
         "specified NUMA node");
+
+    // Add Engram bindings
+    mooncake::engram::bind_engram(m);
 }
 
 }  // namespace mooncake
