4ba5ca487d68e118356f3ea1f78c7731a17b4e2c
fatSheep <tzh2005t@163.com>
2026-01-27 16:53:41 +0800

    test: unittest for etcd backend

--- mooncake-store/include/master_service.h
+++ mooncake-store/include/master_service.h
@@ -1026,6 +1026,7 @@ class MasterService {
     std::shared_ptr<AllocationStrategy> allocation_strategy_;
 
     bool enable_snapshot_restore_ = false;
+    bool enable_snapshot_restore_clean_metadata_ = true;
 
     bool enable_snapshot_ = false;
     std::string snapshot_backup_dir_;
