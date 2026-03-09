0c365fe5b9355e72189a2c4cdb32e06a8108a64f
Xun Sun <UNIDY2002@outlook.com>
2026-03-09 15:11:38 +0800

    Remove 'cuda' library from setup.py [skip ci]
    Removed 'cuda' from the list of libraries in setup.py.

--- mooncake-ep/setup.py
+++ mooncake-ep/setup.py
@@ -33,7 +33,7 @@ setup(
                 "cxx": [f"-D_GLIBCXX_USE_CXX11_ABI={abi_flag}", "-std=c++20", "-O3", "-g0"],
                 "nvcc": [f"-D_GLIBCXX_USE_CXX11_ABI={abi_flag}", "-std=c++20", "-Xcompiler", "-O3", "-Xcompiler", "-g0"],
             },
-            libraries=["ibverbs", "mlx5", "cuda"],
+            libraries=["ibverbs", "mlx5"],
             extra_link_args=[
                 "-Wl,-rpath,$ORIGIN",
                 "-L" + os.path.join(current_dir, "../mooncake-wheel/mooncake"),
