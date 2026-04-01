2369ef4428bed4cddf0ab7907e1a4f7be4ad2735
liam <yzwliam@126.com>
2026-04-01 09:09:01 +0000

    [PG]: update timeout detect interval

--- mooncake-pg/src/mooncake_worker.cu
+++ mooncake-pg/src/mooncake_worker.cu
@@ -63,7 +63,7 @@ class MooncakeBarrierWorkCuda : public MooncakeWorkCuda {
             if (elapsed >= timeout) {
                 return false;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::microseconds(10));
         }
         return true;
     }
