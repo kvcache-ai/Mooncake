a52a89f0168801b4ae9ebdb4364f3d06c4761083
zhang zuoyuan <zhangzuo21@mails.tsinghua.edu.cn>
2026-04-16 13:29:49 +0800

    format

--- mooncake-store/src/real_client_main.cpp
+++ mooncake-store/src/real_client_main.cpp
@@ -116,7 +116,8 @@ int main(int argc, char *argv[]) {
     coro_rpc::coro_rpc_server server(FLAGS_threads, FLAGS_port, FLAGS_host);
     RegisterClientRpcService(server, *client_inst);
 
-    LOG(INFO) << "Starting real client service on " << FLAGS_host << ":" << FLAGS_port;
+    LOG(INFO) << "Starting real client service on " << FLAGS_host << ":"
+              << FLAGS_port;
 
     return server.start();
 }
