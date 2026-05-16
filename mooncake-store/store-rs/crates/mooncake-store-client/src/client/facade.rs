230f7d2899fea88fb21d91a5b95a7ea203a0962b
Kun(llfl) <llfl@linux.alibaba.com>
2026-05-16 20:21:28 +0800

    Revert "[Store-RS] Remove duplicate readable replica helper"
    This reverts commit 5497603f1433040b5697a60e7e0264be0f0ec2fa.
    
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

--- mooncake-store/store-rs/crates/mooncake-store-client/src/client/facade.rs
+++ mooncake-store/store-rs/crates/mooncake-store-client/src/client/facade.rs
@@ -136,6 +136,15 @@ impl StoreClient {
         self.filter_routes_to_readable(routes)
     }
 
+    fn route_has_readable_replica(
+        &self,
+        route: &ObjectRoute,
+        readable_runtimes: &BTreeSet<ClientRuntimeId>,
+    ) -> bool {
+        self.select_readable_replica(route, readable_runtimes)
+            .is_some()
+    }
+
     pub fn batch_is_readable(&self, objects: &[ObjectRef<'_>]) -> Result<Vec<bool>> {
         let keys = objects
             .iter()
