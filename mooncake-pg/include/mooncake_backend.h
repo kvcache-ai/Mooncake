dbffd05b8b9da50f026c968e80ff40180cc6ee27
Zhanhao Cao <cao2013zh@163.com>
2026-07-17 13:11:47 +0800

    Update mooncake-pg/include/mooncake_backend.h
    Co-authored-by: Yue Chen <yuechen.bupt@gmail.com>

--- mooncake-pg/include/mooncake_backend.h
+++ mooncake-pg/include/mooncake_backend.h
@@ -59,8 +59,8 @@ class MooncakeP2PShim final : public ::c10d::Backend {
 
 class MooncakeBackend final : public ::c10d::ProcessGroup {
    public:
-    static constexpr size_t kDefaultCollectiveTimeoutUs = 100000;  // 100 ms
-    static constexpr int64_t kDefaultP2PTimeoutUs = 3000000;       // 3 s
+    static constexpr size_t kDefaultCollectiveTimeoutUs = 10000000;  // 10 s
+    static constexpr int64_t kDefaultP2PTimeoutUs = 10000000;       // 10 s
 
     struct MooncakeBackendOptions final : torch::CustomClassHolder {
         explicit MooncakeBackendOptions(at::Tensor activeRanks)
