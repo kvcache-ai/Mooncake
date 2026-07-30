639e305f4efa68c7983dba2eca27a4aa9a582b42
Yuchen Kou <kouyuchen@approaching.ai>
2026-07-30 13:15:44 +0800

    fix: jsoncpp/json/json.h not found

--- mooncake-store/tests/ha/oplog/oplog_batch_auditor_test.cpp
+++ mooncake-store/tests/ha/oplog/oplog_batch_auditor_test.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
 
-#include <jsoncpp/json/json.h>
+#if __has_include(<jsoncpp/json/json.h>)
+#include <jsoncpp/json/json.h>  // Ubuntu
+#else
+#include <json/json.h>  // CentOS
+#endif
 
 #include <algorithm>
 #include <sstream>
