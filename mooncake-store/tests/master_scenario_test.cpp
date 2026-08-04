56ed12468afb5f056245309a5f775fa467164fbd
Aionw <aione.moe.dev@gmail.com>
2026-08-04 15:03:01 +0800

    test(store): split scenario behavior target

--- mooncake-store/tests/master_scenario_test.cpp
+++ mooncake-store/tests/master_scenario_test.cpp
@@ -51,28 +51,6 @@ static_assert(SupportsThen<ReadableObject>);
 
 }  // namespace
 
-TEST(MasterServiceTest, PutStartEndFlow) {
-    MasterScenario("put start/end flow")
-        .Given(MemoryNode("memory"))
-        .When(PutStart("test_key", 1_KB)
-                  .By("writer")
-                  .ExpectReplicas(1)
-                  .ExpectStatus(ReplicaStatus::PROCESSING))
-        .Then(Object("test_key").IsNotReady())
-        .When(Remove("test_key").ExpectError(ErrorCode::REPLICA_IS_NOT_READY))
-        .When(PutEnd("test_key")
-                  .By("other-writer")
-                  .ExpectError(ErrorCode::ILLEGAL_CLIENT))
-        .When(PutRevoke("test_key")
-                  .By("other-writer")
-                  .ExpectError(ErrorCode::ILLEGAL_CLIENT))
-        .When(PutEnd("test_key").By("writer"))
-        .Then(Object("test_key")
-                  .IsReadable()
-                  .HasReplicas(1)
-                  .HasCompleteReplicas(1));
-}
-
 TEST(MasterScenarioContractTest, ReportsUnexpectedActionError) {
     EXPECT_NONFATAL_FAILURE(MasterScenario("unexpected action error")
                                 .Given(MemoryNode("memory"))
