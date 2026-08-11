2eb9487601e125f2b3c64f5af19f4247cd8cc13d
Aionw <aione.moe.dev@gmail.com>
2026-08-11 16:56:24 +0800

    [Store] Remove unused LRU eviction strategy

--- mooncake-store/include/eviction_strategy.h
+++ mooncake-store/include/eviction_strategy.h
@@ -40,42 +40,6 @@ class EvictionStrategy : public std::enable_shared_from_this<EvictionStrategy> {
         all_key_idx_map_;
 };
 
-class LRUEvictionStrategy : public EvictionStrategy {
-   public:
-    virtual ErrorCode AddKey(const std::string& key) override {
-        // Add key to the front of the list
-        if (all_key_idx_map_.find(key) != all_key_idx_map_.end()) {
-            all_key_list_.erase(all_key_idx_map_[key]);
-            all_key_idx_map_.erase(key);
-        }
-        all_key_list_.push_front(key);
-        all_key_idx_map_[key] = all_key_list_.begin();
-        return ErrorCode::OK;
-    }
-
-    virtual ErrorCode UpdateKey(const std::string& key) override {
-        // Move the key to the front of the list
-        auto it = all_key_idx_map_.find(key);
-        if (it != all_key_idx_map_.end()) {
-            all_key_list_.erase(it->second);
-            all_key_list_.push_front(key);
-            all_key_idx_map_[key] = all_key_list_.begin();
-        }
-        return ErrorCode::OK;
-    }
-
-    virtual std::string EvictKey(void) override {
-        // Evict the last key in the list
-        if (all_key_list_.empty()) {
-            return "";
-        }
-        std::string evicted_key = all_key_list_.back();
-        all_key_list_.pop_back();
-        all_key_idx_map_.erase(evicted_key);
-        return evicted_key;
-    }
-};
-
 class FIFOEvictionStrategy : public EvictionStrategy {
    public:
     virtual ErrorCode AddKey(const std::string& key) override {
--- mooncake-store/tests/eviction_strategy_test.cpp
+++ mooncake-store/tests/eviction_strategy_test.cpp
@@ -23,57 +23,6 @@ class EvictionStrategyTest : public ::testing::Test {
     }
 };
 
-// Test LRUEvictionStrategy AddKey and RemoveKey functionality
-TEST_F(EvictionStrategyTest, AddAndRemoveKey) {
-    LRUEvictionStrategy eviction_strategy;
-
-    // Add keys
-    EXPECT_EQ(eviction_strategy.AddKey("key1"), ErrorCode::OK);
-    EXPECT_EQ(eviction_strategy.AddKey("key2"), ErrorCode::OK);
-
-    // Check size
-    EXPECT_EQ(eviction_strategy.GetSize(), 2);
-
-    // Remove a key
-    EXPECT_EQ(eviction_strategy.RemoveKey("key1"), ErrorCode::OK);
-    EXPECT_EQ(eviction_strategy.GetSize(), 1);
-
-    // Clean up
-    eviction_strategy.CleanUp();
-}
-
-// Test LRUEvictionStrategy EvictKey functionality
-TEST_F(EvictionStrategyTest, EvictKey) {
-    LRUEvictionStrategy eviction_strategy;
-
-    // Add keys
-    EXPECT_EQ(eviction_strategy.AddKey("key1"), ErrorCode::OK);
-    EXPECT_EQ(eviction_strategy.AddKey("key2"), ErrorCode::OK);
-
-    // Evict a key
-    std::string evicted_key = eviction_strategy.EvictKey();
-    EXPECT_NE(evicted_key, "");
-
-    // Check if the evicted key is equal to the expected key
-    EXPECT_EQ(evicted_key, "key1");
-
-    // Now we add more keys and update some of them
-    EXPECT_EQ(eviction_strategy.AddKey("key3"), ErrorCode::OK);
-    EXPECT_EQ(eviction_strategy.AddKey("key4"), ErrorCode::OK);
-    EXPECT_EQ(eviction_strategy.UpdateKey("key2"), ErrorCode::OK);
-    EXPECT_EQ(eviction_strategy.UpdateKey("key3"), ErrorCode::OK);
-
-    // Evict another key
-    evicted_key = eviction_strategy.EvictKey();
-    EXPECT_NE(evicted_key, "");
-    // Check if the evicted key is equal to the expected key
-    EXPECT_EQ(evicted_key, "key4");
-    // Check size
-    EXPECT_EQ(eviction_strategy.GetSize(), 2);
-    // Clean up
-    eviction_strategy.CleanUp();
-}
-
 // Test FIFOEvictionStrategy AddKey and RemoveKey functionality
 TEST_F(EvictionStrategyTest, FIFOAddAndRemoveKey) {
     FIFOEvictionStrategy eviction_strategy;
