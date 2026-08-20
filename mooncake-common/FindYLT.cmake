0903fcf9dfa6c14cb24124692f6692f9276dd4f8
ur4t <46435411+ur4t@users.noreply.github.com>
2026-08-20 11:18:55 +0000

    Add github mirror

--- mooncake-common/FindYLT.cmake
+++ mooncake-common/FindYLT.cmake
@@ -6,10 +6,12 @@ if(POLICY CMP0135)
   cmake_policy(SET CMP0135 NEW)
 endif()
 
+set(GH_URL $ENV{ASCEND_GITHUB_MIRROR_URLS})
+
 set(YLT_VERSION 0.5.7)
 FetchContent_Declare(
   yalantinglibs
-  URL https://github.com/alibaba/yalantinglibs/archive/refs/tags/${YLT_VERSION}.tar.gz
+  URL ${GH_URL}https://github.com/alibaba/yalantinglibs/archive/refs/tags/${YLT_VERSION}.tar.gz
   URL_HASH
     SHA256=1c1057289e5488f90dd326fd2bb9d3173bad11eb5b06bc0a8bf0fa80857e1cfa
 )
