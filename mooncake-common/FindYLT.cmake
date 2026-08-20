3531f288631c3a75b5829c0dfe4c7e8169819546
ur4t <46435411+ur4t@users.noreply.github.com>
2026-08-20 11:09:18 +0000

    Fix Asio and exclude YLT from install

--- mooncake-common/FindYLT.cmake
+++ mooncake-common/FindYLT.cmake
@@ -6,25 +6,6 @@ if(POLICY CMP0135)
   cmake_policy(SET CMP0135 NEW)
 endif()
 
-set(BUILD_EXAMPLES
-    OFF
-    CACHE BOOL "Build YLT examples")
-set(BUILD_BENCHMARK
-    OFF
-    CACHE BOOL "Build YLT benchmarks")
-set(BUILD_UNIT_TESTS
-    OFF
-    CACHE BOOL "Build YLT unittests")
-set(INSTALL_THIRDPARTY
-    OFF
-    CACHE BOOL "Install YLT thirdparty")
-set(INSTALL_STANDALONE
-    OFF
-    CACHE BOOL "Install YLT standalone")
-set(INSTALL_INDEPENDENT_STANDALONE
-    OFF
-    CACHE BOOL "Install YLT independent standalone")
-
 set(YLT_VERSION 0.5.7)
 FetchContent_Declare(
   yalantinglibs
@@ -33,4 +14,11 @@ FetchContent_Declare(
     SHA256=1c1057289e5488f90dd326fd2bb9d3173bad11eb5b06bc0a8bf0fa80857e1cfa
 )
 
-FetchContent_MakeAvailable(yalantinglibs)
+# Exclude from install for CMake before 3.28
+FetchContent_GetProperties(yalantinglibs)
+FetchContent_Populate(yalantinglibs)
+add_subdirectory(
+    ${yalantinglibs_SOURCE_DIR}
+    ${yalantinglibs_BINARY_DIR}
+    EXCLUDE_FROM_ALL
+)
