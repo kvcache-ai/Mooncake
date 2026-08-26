5b1305cc2c43501e5a4d62d3cb28576bacd344c5
ur4t <46435411+ur4t@users.noreply.github.com>
2026-08-26 12:52:28 +0000

    Use offical Go tarball

--- scripts/install_toolchain.sh
+++ scripts/install_toolchain.sh
@@ -3,9 +3,13 @@
 set -e
 set -u
 
-MOONCAKE_TOOLCHAIN=/opt/mooncake-toolchain
+MOONCAKE_TOOLCHAIN="/opt/mooncake-toolchain"
+
+GO_DL_HOST="go.dev"
+GO_ARCH="unknown"
+GO_VERSION="go1.27.0"
+
 GITHUB_MIRROR=${ASCEND_GITHUB_MIRROR_URLS:-}
-AUDIT_GO_WHEEL=${AUDIT_GO_WHEEL:-}
 
 PATH=${MOONCAKE_TOOLCHAIN}/bin:$PATH
 
@@ -18,13 +22,14 @@ python3 -m venv ${MOONCAKE_TOOLCHAIN}
 test -z "$GITHUB_MIRROR" || python3 -m pip config --user set \
   global.index-url https://mirrors.huaweicloud.com/repository/pypi/simple
 python3 -m pip install --upgrade pip cmake ninja
-python3 -m pip install go-bin==1.27.0
 
-test -z "$AUDIT_GO_WHEEL" && exit
-
-cd $(find ${MOONCAKE_TOOLCHAIN} -type d -name go)
-find -type f -exec sha256sum {} \; | tee /tmp/wheel.sha256sum
-wget -q -O - https://go.dev/dl/go1.27.0.linux-amd64.tar.gz | tar -C /tmp -xzf -
-cd /tmp/go
-find -type f -exec sha256sum {} \; | tee /tmp/tar.sha256sum
-diff -u /tmp/tar.sha256sum /tmp/wheel.sha256sum | tee /tmp/audit.diff
+command -v go && test "$(go env GOVERSION)" = "$GO_VERSION" && exit
+ARCH=$(uname -m)
+test "$ARCH" = "x86_64" && GO_ARCH="amd64" || :
+test "$ARCH" = "aarch64" && GO_ARCH="arm64" || :
+test "$GO_ARCH" = "unknown" && echo "Unsupported architecture: $ARCH" && exit 1
+test -z "$GITHUB_MIRROR" || GO_DL_HOST="golang.google.cn"
+wget -q -O - "https://$GO_DL_HOST/dl/$GO_VERSION.linux-$GO_ARCH.tar.gz" | \
+  tar -C ${MOONCAKE_TOOLCHAIN} -x -z -f -
+ln -s ../go/bin/go ${MOONCAKE_TOOLCHAIN}/bin/go
+ln -s ../go/bin/gofmt ${MOONCAKE_TOOLCHAIN}/bin/gofmt
