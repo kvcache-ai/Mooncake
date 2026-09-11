75134a9ae2231f4d08699b1458e6ba93fd784b90
Xun Sun <UNIDY2002@outlook.com>
2026-09-11 22:39:41 +0800

    test(ep): allow separate metadata group backend

--- python/tests/ep/ep_test_utils.py
+++ python/tests/ep/ep_test_utils.py
@@ -28,7 +28,10 @@ def init_dist(local_rank: int, num_local_ranks: int):
     torch.set_default_dtype(torch.bfloat16)
     torch.set_default_device("cuda")
 
-    group = dist.new_group(list(range(num_local_ranks * num_nodes)))
+    group_backend = os.getenv("MOONCAKE_EP_TEST_GROUP_BACKEND")
+    group = dist.new_group(
+        list(range(num_local_ranks * num_nodes)), backend=group_backend
+    )
     return dist.get_rank(), dist.get_world_size(), group
 
 
