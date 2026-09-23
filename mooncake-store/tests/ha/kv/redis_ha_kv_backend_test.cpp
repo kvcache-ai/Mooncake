074a214f3f0219349f689db16271afcfbf054b32
w6910502 <wangjiandong@kylinos.cn>
2026-09-23 20:09:41 +0800

    [Store] HA optlog support redis backed

--- mooncake-store/tests/ha/kv/redis_ha_kv_backend_test.cpp
+++ mooncake-store/tests/ha/kv/redis_ha_kv_backend_test.cpp
@@ -213,3 +213,11 @@ TEST_F(RedisOpLogBackendTest, StorageWritesAndReadsContiguousBatches) {
 #endif
 
 }  // namespace mooncake::test
+
+#ifdef STORE_USE_REDIS
+int main(int argc, char** argv) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+#endif
